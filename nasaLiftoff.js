7f84e1d101e8dff7fe253547241eb3420bf28b27
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-05 15:24:44 -0400

    Complete code for rocket launch count down

---
+++ nasaLiftoff.js
@@ -0,0 +1,26 @@
+let countdown = 60;
+while (countdown >= 0) {
+  switch (countdown) {
+    case 50:
+      console.log(`Orbiter transfers from ground to internal power`);
+      break;
+    case 31:
+      console.log('Ground launch sequencer is go for auto sequence start');
+      break;
+    case 16:
+      console.log(`Activate launch pad sound suppression system`);
+      break;
+    case 10:
+      console.log(`Activate main engine hydrogen burnoff system`);
+      break;
+    case 6:
+      console.log(`Main engine start`);
+      break;
+    case 0:
+      console.log(`Solid rocket booster ignition and liftoff!`);
+      break;
+    default:
+      console.log(`T-${countdown}`);
+  }
+  countdown -= 1;
+}
