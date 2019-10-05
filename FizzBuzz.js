d660b5b1b55f9a8c8007ad805e4e7b6e0db7a630
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-05 14:40:27 -0400

    Completed FizzBuzz style algorithm

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -3,9 +3,18 @@
  */
 
 var x = 1;
-
-while (/* your stop condition goes here */) {
+let printStatement = "";
+while (x <= 20) {
     // check divisibility
     // print Julia, James, or JuliaJames
     // increment x
+    if (x % 3 === 0) {
+      printStatement = "Julia";
+    }
+    if (x % 5 === 0) {
+      printStatement += "James";
+    }
+    console.log(printStatement || x);
+    printStatement = "";
+    x += 1;
 }
