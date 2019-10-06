9090a601cd72d21db3a95262e44f31e7c306207e
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-06 14:33:15 -0400

    Creates a named function expression.

---
+++ namedFunctionExpression.js
@@ -0,0 +1,8 @@
+/* Create a named function expression that stores the function
+in a variable called "cry" and returns "boohoo!". */
+
+var cry = function crying() {
+  return "boohoo!";
+}
+
+console.log(cry());
