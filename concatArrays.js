13c4173676b3f8fbb19012bf070279a5cca2d6a1
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-07 23:44:08 -0400

    Function to concatenate two arrays

---
+++ concatArrays.js
@@ -0,0 +1,14 @@
+// Concatenate two arrays
+
+function concat(array1, array2) {
+  // array1.push(array2);
+  for (var x = 0; x < array2.length; x++) {
+    array1.push(array2[x]);
+  }
+  return array1;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
