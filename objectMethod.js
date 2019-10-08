bf30f696101773c93c3b83071836f9ce41a3ed0d
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-08 08:41:24 -0400

    Adds method to an object.

---
+++ objectMethod.js
@@ -0,0 +1,28 @@
+/*
+ * Programming Quiz: Umbrella (7-1)
+ * Add a method for closing the umbrella.
+ */
+
+var umbrella = {
+  color: "pink",
+  isOpen: true,
+  open: function() {
+      if (umbrella.isOpen === true) {
+          return "The umbrella is already opened!";
+      } else {
+          umbrella.isOpen = true;
+          return "Julia opens the umbrella!";
+      }
+  },
+  close: function() {
+    if (umbrella.isOpen === false) {
+      return "The umbrella is already closed!";
+    } else {
+      umbrella.isOpen = false;
+      return "Julia closes the umbrella!";
+    }
+  }
+};
+
+console.log(umbrella.isOpen);
+console.log(umbrella.close());
