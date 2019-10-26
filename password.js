46927516d30c81a128a4bd45e55d64899226cfc9
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-25 23:32:30 -0400

    Adds command line argument for user input of original password.

--- password.js
+++ password.js
@@ -1,25 +1,30 @@
 // Function to obfuscator a passed string
 
+let oldPassword = process.argv[2];
+
 function obfuscate(password) {
-  let newPass = "";
-  for (let char of password) {
-    switch(char) {
-      case "a":
-        char = "4";
-        break;
-      case "e":
-        char = "3";
-        break;
-      case "o":
-        char = "0";
-        break;
-      case "l":
-        char = "1";
-        break;
+  if (password && (typeof password === "string")) {
+    let newPass = "";
+    for (let char of password) {
+      switch(char) {
+        case "a":
+          char = "4";
+          break;
+        case "e":
+          char = "3";
+          break;
+        case "o":
+          char = "0";
+          break;
+        case "l":
+          char = "1";
+          break;
+      }
+      newPass += char;
     }
-    newPass += char;
+    return newPass;
   }
-  return newPass;
+  return "Please enter a valid password";
 }
 
-console.log(obfuscate("password"));
+console.log(obfuscate(oldPassword));
