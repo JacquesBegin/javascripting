881fb81b634293526fdc565855bae002cfc419ff
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-25 23:22:55 -0400

    Adds switch statement to handle which characters to replace.

--- password.js
+++ password.js
@@ -1,5 +1,22 @@
 // Function to obfuscator a passed string
 
-function obfuscator(password) {
-  
+function obfuscate(password) {
+  for (let char of password) {
+    switch(char) {
+      case "a":
+        char = "4";
+        break;
+      case "e":
+        char = "3";
+        break;
+      case "o":
+        char = "0";
+        break;
+      case "l":
+        char = "1";
+        break;
+    }
+  }
+  return password;
 }
+
